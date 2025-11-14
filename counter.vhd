279bbd46f47d1280815f6e094666d95ae9bd3242
Dante Pedrosa <dantepedrosa@gmail.com>
2025-11-14 08:41:59 -0300

    Counter alterado

--- counter.vhd
+++ counter.vhd
@@ -5,39 +5,42 @@ use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
 entity counter is
+    generic (
+        N : integer := 8               -- numero de bits
+    );
     port (
-        CLEAR : in std_logic;                       -- reset assincrono
-        CLK   : in std_logic;                       -- clock
-        COUNT : in std_logic;                       -- habilita incremento
-        LD    : in std_logic;                       -- load sincron o
-        DIN   : in std_logic_vector(7 downto 0);    -- valor para carregar
-        Q     : out std_logic_vector(7 downto 0);   -- saida do contador
-        COUT  : out std_logic                       -- carry-out quando atinge o valor maximo
+        CLEAR : in  std_logic;                     -- reset assincrono
+        CLK   : in  std_logic;                     -- clock
+        COUNT : in  std_logic;                     -- enable de incremento
+        LD    : in  std_logic;                     -- load sincrono
+        DIN   : in  std_logic_vector(N-1 downto 0);-- valor de entrada
+        Q     : out std_logic_vector(N-1 downto 0);-- saida
+        COUT  : out std_logic                      -- carry out
     );
 end counter;
 
 architecture my_count of counter is
-    signal t_cnt : unsigned(7 downto 0); -- contador interno
+    signal t_cnt : unsigned(N-1 downto 0);         -- registrador interno
 begin
 
     process (CLK, CLEAR)
     begin
         if (CLEAR = '1') then
-            t_cnt <= (others => '0');            -- clear assincrono
+            t_cnt <= (others => '0');              -- reset
 
         elsif rising_edge(CLK) then
             if (LD = '1') then
-                t_cnt <= unsigned(DIN);          -- load sincron o
+                t_cnt <= unsigned(DIN);            -- load
             elsif (COUNT = '1') then
-                t_cnt <= t_cnt + 1;              -- incrementa
+                t_cnt <= t_cnt + 1;                -- incremento
             end if;
         end if;
     end process;
 
-    -- saida do contador
+    -- saida Q
     Q <= std_logic_vector(t_cnt);
 
-    -- carry-out quando atinge o valor maximo
-    COUT <= '1' when t_cnt = x"FF" and COUNT = '1' else '0';
+    -- carry-out quando atingir o valor maximo
+    COUT <= '1' when t_cnt = (2**N - 1) and COUNT = '1' else '0';
 
 end my_count;
