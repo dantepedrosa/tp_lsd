ac4f212b711b86e69a3e70333eb8fd3fe7332112
Dante Pedrosa <dantepedrosa@gmail.com>
2025-11-14 12:56:25 -0300

    aJUSTE NA FSM

--- fsm.vhd
+++ fsm.vhd
@@ -16,21 +16,18 @@ end fsm;
 
 architecture rtl of fsm is
 
+    -- NOMEACAO DOS ESTADOS
     type state_type is (
-        ST_WATCH,
-        ST_STPWATCH_IDLE,
-        ST_STPWATCH_RUN,
-        ST_SET_HOUR,
-        ST_SET_MIN
+        ST_RELOGIO,
+        ST_CRONOMETRO_STOP,
+        ST_CRONOMETRO_RUN,
+        ST_AJUSTA_HORA,
+        ST_AJUSTA_MIN
     );
 
     signal PS, NS : state_type;
 
 begin
-
-    ----------------------------------------------------------------
-    -- process sincronizado: transicao PS <= NS
-    ----------------------------------------------------------------
     sync_p: process (clk)
     begin
         if rising_edge(clk) then
@@ -38,12 +35,9 @@ begin
         end if;
     end process sync_p;
 
-    ----------------------------------------------------------------
-    -- process combinacional: define saidas e proximo estado
-    ----------------------------------------------------------------
     comb_p: process (PS, btn_mode, btn_action)
     begin
-        -- valores padrao
+        -- inicializacao das variaveis de saida
         watch_mode <= '1';
         stpwtch_en <= '0';
         set_hour   <= '0';
@@ -52,69 +46,62 @@ begin
 
         case PS is
 
-            --------------------------------------------------------
-            -- MODO RELogio
-            --------------------------------------------------------
-            when ST_WATCH =>
+            -- ESTADO - RELOGIO CONTANDO
+            when ST_RELOGIO =>
                 watch_mode <= '1';
 
                 if btn_mode = '1' then
-                    NS <= ST_STPWATCH_IDLE;
+                    NS <= ST_CRONOMETRO_STOP;
                 end if;
 
-            --------------------------------------------------------
-            -- CRONOMETRO PARADO
-            --------------------------------------------------------
-            when ST_STPWATCH_IDLE =>
+            
+            -- ESTADO - CRONOMETRO PARADO
+            when ST_CRONOMETRO_STOP =>
                 watch_mode <= '0';
                 stpwtch_en <= '0';
 
                 if btn_action = '1' then
-                    NS <= ST_STPWATCH_RUN;
+                    NS <= ST_CRONOMETRO_RUN;
                 elsif btn_mode = '1' then
-                    NS <= ST_SET_HOUR;
+                    NS <= ST_AJUSTA_HORA;
                 end if;
 
-            --------------------------------------------------------
-            -- CRONOMETRO RODANDO
-            --------------------------------------------------------
-            when ST_STPWATCH_RUN =>
+            -- ESTADO - CRONOMETRO CONTANDO
+            when ST_CRONOMETRO_RUN =>
                 watch_mode <= '0';
                 stpwtch_en <= '1';
 
                 if btn_action = '1' then
-                    NS <= ST_STPWATCH_IDLE;
+                    NS <= ST_CRONOMETRO_STOP;
                 end if;
 
-            --------------------------------------------------------
-            -- AJUSTE DE HORAS
-            --------------------------------------------------------
-            when ST_SET_HOUR =>
+            -- ESTADO - SETAR HORAS
+            -- Obs.: Apenas habilita a edicao de horas. Incremento feito diretamente pelo botao
+            when ST_AJUSTA_HORA =>
                 watch_mode <= '1';
                 set_hour   <= '1';
 
                 if btn_mode = '1' then
-                    NS <= ST_SET_MIN;
+                    NS <= ST_AJUSTA_MIN;
                 elsif btn_action = '1' then
-                    NS <= ST_SET_HOUR; -- incrementa horas externamente
+                    NS <= ST_AJUSTA_HORA; 
                 end if;
 
-            --------------------------------------------------------
-            -- AJUSTE DE MINUTOS
-            --------------------------------------------------------
-            when ST_SET_MIN =>
+            -- ESTADO - SETAR MINUTOS
+            -- Obs.: Apenas habilita a edicao de minutos. Incremento feito diretamente pelo botao
+            when ST_AJUSTA_MIN =>
                 watch_mode <= '1';
                 set_min    <= '1';
 
                 if btn_mode = '1' then
-                    NS <= ST_WATCH;
+                    NS <= ST_RELOGIO;
                 elsif btn_action = '1' then
-                    NS <= ST_SET_MIN; -- incrementa minutos externamente
+                    NS <= ST_AJUSTA_MIN;
                 end if;
 
-            --------------------------------------------------------
+            -- ESTADO - PADRAO
             when others =>
-                NS <= ST_WATCH;
+                NS <= ST_RELOGIO;
 
         end case;
     end process comb_p;
